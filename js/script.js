b6055183ffdee056f4a156aa405d6c7f5b7401f2
alaansabu <alansabu6046@gmail.com>
2024-10-24 21:21:33 +0530

    Initial commit

--- js/script.js
+++ js/script.js
@@ -1,30 +1,39 @@
-let firstcard =11;
-let secondcard=10;
-let sum= firstcard+secondcard;
-hasBlackjack = false;
-isAlive = true;
- let messageEl=document.getElementById("message-el") 
- let message;
- let sumEl=document.getElementById("sum-el")
- let cardEl=document.getElementById("card-el")
+let firstcard = 11;
+let secondcard = 10;
+let sum = firstcard + secondcard;
+let hasBlackjack = false;
+let isAlive = true;
+
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardEl = document.getElementById("card-el");
+
+let message;
+
 function start() {
-    
-if (sum < 21){
-message="Do you want to pick a new card";
+    if (sum < 21) {
+        message = "Do you want to pick a new card?";
+    } else if (sum === 21) {
+        message = "Yay! You have won Blackjack!";
+        hasBlackjack = true;
+    } else if (sum > 21) {
+        message = "You lost! Try again.";
+        isAlive = false;
+    }
+
+    sumEl.textContent = "Sum: " + sum;
+    cardEl.textContent = "Cards: " + firstcard + " " + secondcard;
+    messageEl.textContent = message;
 }
 
-else if (sum === 21) {
-    message = "Yay! You have won Blackjack!";
-    hasBlackjack = true;
-}
+function newcard() {
+    console.log("Selecting a new card");
+    let newcard = 5; 
+    sum += newcard;
 
+    message = "New card drawn!";
 
-else if (sum >21){
-    message="FUCK OFF "
-    isAlive=false
-}
-
-sumEl.textContent="cards :"+ " "+firstcard+" "+secondcard;
-cardEl.textContent="sum :"+ " "+sum
-messageEl.textContent=message;
+    sumEl.textContent = "Sum: " + sum;
+    cardEl.textContent = "Cards: " + firstcard + " " + secondcard + " " + newcard;
+    messageEl.textContent = message;
 }
